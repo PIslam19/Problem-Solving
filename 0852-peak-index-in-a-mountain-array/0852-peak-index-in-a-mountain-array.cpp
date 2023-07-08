3264bd26354d16a33f94dcc37db48f9c751693db
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-07-08 16:15:06 +0600

    Time: 124 ms (66.94%), Space: 59.6 MB (91.45%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size();
-        int m=0,t;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]>m)
-            {
-                m=arr[i];
-                t=i;
-            }
-        }
-        return t;
+        return max_element( arr.begin(), arr.end() )-arr.begin() ;
     }
 };
