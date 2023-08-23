6ca276a901003d40085fbfcbb1a8c262382d8b96
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-08-23 12:34:10 +0600

    Time: 116 ms (67.71%), Space: 71.7 MB (13.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-      set<int> s;
-        for(auto u: nums) s.insert(u);
-        if(nums.size()==s.size())return false;
-        else return true;
+      unordered_map<int,int> mp;
+          for(auto i : nums) mp[i]++;
+          bool flag = false;
+          for(auto i : mp){
+              if(i.second >= 2) return true;
+          }
+          return flag;
         
     }
 };
