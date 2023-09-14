1bcb50f1b914121407d09f4e7ec63f2800b81d91
Md Islam Sardar <mdalislam112786@gmail.com>
2023-09-14 10:43:25 +0600

    Time: 83 ms (63.56%), Space: 68.1 MB (6.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
         int a=0,b=INT_MIN;
         for(auto u:nums)
         {
