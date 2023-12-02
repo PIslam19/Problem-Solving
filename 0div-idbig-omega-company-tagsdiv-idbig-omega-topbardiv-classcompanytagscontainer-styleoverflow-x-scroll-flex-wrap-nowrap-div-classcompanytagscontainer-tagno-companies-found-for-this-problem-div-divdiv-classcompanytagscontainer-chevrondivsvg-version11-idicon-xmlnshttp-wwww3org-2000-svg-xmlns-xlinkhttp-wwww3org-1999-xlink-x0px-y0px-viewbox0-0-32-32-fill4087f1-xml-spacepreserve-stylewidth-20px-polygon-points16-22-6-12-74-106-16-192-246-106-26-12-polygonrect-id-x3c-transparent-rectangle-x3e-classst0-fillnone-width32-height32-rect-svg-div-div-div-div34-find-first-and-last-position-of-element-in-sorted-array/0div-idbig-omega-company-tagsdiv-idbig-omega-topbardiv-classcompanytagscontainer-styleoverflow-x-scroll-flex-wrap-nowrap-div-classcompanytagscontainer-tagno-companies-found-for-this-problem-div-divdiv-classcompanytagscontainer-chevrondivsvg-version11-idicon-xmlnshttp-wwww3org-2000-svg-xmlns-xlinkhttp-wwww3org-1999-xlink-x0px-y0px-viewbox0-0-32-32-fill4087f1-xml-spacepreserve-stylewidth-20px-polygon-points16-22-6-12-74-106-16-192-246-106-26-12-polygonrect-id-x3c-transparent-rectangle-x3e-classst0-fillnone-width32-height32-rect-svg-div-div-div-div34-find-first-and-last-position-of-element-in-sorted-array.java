bf515286b3c92e793d41673b93fc4ddc5784d2a8
Md Islam Sardar <mdalislam112786@gmail.com>
2023-12-02 17:52:14 +0600

    Time: 0 ms (100.00%), Space: 44.2 MB (79.04%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int search(int []ar, int tar, boolean ffi)
+    public int bs(int []ar,int tar,boolean iff)
     {
         int ans=-1;
-        int start=0;
-        int end=ar.length -1;
+        int start=0,end=ar.length-1;
         while(start<=end)
         {
-            int mid=start+(end-start)/2;
-            if(ar[mid]<tar)
-            {
-                start=mid+1;
-            }
-            else if(ar[mid]>tar)
+            int mid =start +(end-start)/2;
+            if(ar[mid]>tar)
             {
                 end=mid-1;
+            }
+            else if(ar[mid]<tar)
+            {
+                start=mid+1;
             }
             else
             {
                 ans=mid;
-                if(ffi)
+                if(iff)
                 {
                     end=mid-1;
                 }
@@ -31,11 +30,9 @@ class Solution {
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        int []ans={-1,-1};
-        int start= search(nums,target,true);
-        int end=search (nums,target,false);
-        ans[0]=start;
-        ans[1]=end;
-        return ans;
+        int [] serch={-1,-1};
+        serch[0]=bs(nums,target,true);
+        serch[1]=bs(nums,target,false);
+        return serch;
     }
 }
